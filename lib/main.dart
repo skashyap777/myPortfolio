d1a53f23cc7aaa5255d9015fe4deebdabfc9f9e1
Deadly <samudrakashyap777@gmail.com>
2024-12-20 22:20:26 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/2677048.jfif"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 35.0, left: 20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60, backgroundImage: AssetImage("images/sam.jfif"),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Samudra Kashyap",
+                        style: TextStyle(fontSize: 25,color: Colors.white, fontFamily: "Code"),
+                      ),
+                      Text(
+                        "Devloper",
+                        style: TextStyle(color: Colors.white,fontSize: 15, fontFamily: "Code"),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.school,
+
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "B.Tech in CSE",
+                          style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.computer_rounded,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Fluuter, Flask",
+                          style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_pin,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "GUWAHATI",
+                          style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "skashyap@gmail.com",
+                          style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "789617104",
+                          style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Debugging is like being the detective in a crime movie where you are also the murderer.",style: TextStyle(color: Colors.white,fontSize: 22, fontFamily: "Code"),),
+              ),
+              SizedBox(height: 200,),
+              Text("Created By Sam",style: TextStyle(color: Colors.white, fontFamily: "Code"),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
